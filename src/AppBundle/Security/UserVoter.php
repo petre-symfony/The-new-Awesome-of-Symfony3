2e58c6750d51b4fe3d6fa115df763abc7b1000bb
petrero <petrero@gmail.com>
2017-06-18 13:37:29 +0300

    Adding Voter Logic
    So let's code: In supports(), if (attribute != 'USER_VIEW'), then return false. This says: 'I don't know, go bother some other voter! '.
    
    Add another if statement. Wait! Change the argument to  - this is the object - if any - that's passed to isGranted(). Some now-fixed bad PHP-Doc in Symfony caused that issue.
    
    Anyways, if (! {dollar_sign}object instanceof User), then also return false: we only vote on User objects. Finally, at the bottom, return true
    
    You can of course make your voter support multiple attributes like USER_EDIT or even multiple objects. I usually have one voter per object.
    
    If you return true from supports(), then voteOnAttribute() is called. This is where you shine: do whatever crazy business logic you need to and ultimately return true for access or false to deny access. The {dollar_sign}attribute and {dollar_sign}object are the same as before and  gives you access to the currently-logged-in user.

--- src/AppBundle/Security/UserVoter.php
+++ src/AppBundle/Security/UserVoter.php
@@ -3,10 +3,19 @@ namespace AppBundle\Security;
 
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use AppBundle\Entity\User;
 
 class UserVoter extends Voter{
-  protected function supports($attribute, $subject) {
+  protected function supports($attribute, $object) {
+    if ($attribute != 'USER_VIEW'){
+      return false;
+    }
     
+    if (!$object instanceof User){
+      return FALSE;
+    }
+    
+    return true;
   }
 
   protected function voteOnAttribute($attribute, $subject, TokenInterface $token) {
