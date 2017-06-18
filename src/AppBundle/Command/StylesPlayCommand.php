8a66b96393c4c989a7c388a8b18e1d7fe07bc881
petrero <petrero@gmail.com>
2017-06-18 13:09:14 +0300

    Copy code from branch 7

---
+++ src/AppBundle/Command/StylesPlayCommand.php
@@ -0,0 +1,45 @@
+<?php
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class StylesPlayCommand extends ContainerAwareCommand{
+  protected function configure() {
+    $this->setName('styles:play');
+  }
+  
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $style = new SymfonyStyle($input, $output);
+    $style->title('Welcome to SymfonyStyle!');
+    $style->section('Wow, look at this text section');
+    $style->text('Lorem Lipsum Dolor! Lorem Lipsum Dolor! Lorem Lipsum Dolor!');
+    $style->note('More sure write some *real* text eventually');
+    $style->comment('Lorem Lipsum is just latin garbage');
+    $style->comment('Don\'t overuse it');
+    
+    $style->section('How about some BIG messages?');
+    $style->success('I <3 lorem lipsum');
+    $style->warning('We should *maybe* not use Lorem Lipsum');
+    $style->error('You should stop using Lorem Lipsum');
+    $style->caution('STOP USING IT SRSLY');
+    
+    $style->section('Some tables and lists?');
+    $style->table(
+      ['User', 'Birthday'], 
+      [
+        ['petrero', 'Sep 23th'] ,
+        ['leanapelham', 'All February']  
+      ]
+    );
+    
+    $style->section('Ryan\'s favorite things');
+    $style->listing([
+      'Travelling',
+      'Pizza',
+      'Rafting'
+    ]);
+  }  
+}
